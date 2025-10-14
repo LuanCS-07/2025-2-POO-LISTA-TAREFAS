e0aeef14f7aae77e0893351704409455932abd14
Luan Conrado <luanconrado07@gmail.com>
2025-10-14 00:44:06 +0000

    Feat:Tarefas

--- tarefas/Operacoes.cs
+++ tarefas/Operacoes.cs
@@ -1,3 +1,4 @@
+using MySql.Data.MySqlClient;
 public class Operacoes
 {
     private string connectionString = "";
--- tarefas/Program.cs
+++ tarefas/Program.cs
@@ -1,2 +1,19 @@
 ﻿
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Preenchendo daos tarefa 01");
+var tarefa01 = new Tarefa ();
+tarefa01.Nome = "Fazer compras";
+tarefa01.Descricao = "Comprar arroz, feijão e frutas";
+tarefa01.DataCriacao = DateTime.now;
+tarefa01.Status = 1;
+tarefa01.DataExecucao = null;
+
+Console.WriteLine("Dados tarefa 01 preenchidos");
+Console.WriteLine("Inserindo dados no banco de dados");
+
+var operacoes = new Operacoes ();
+int idInserido = operacoes.Criar(tarefa01);
+
+Console.WriteLine($"Dados inserindos no banco de dados com sucesso. Id: (idInserindo)");
+
